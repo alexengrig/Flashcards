2a8662a72a79122118211e7c8475af4f7278638f
Grig Alex <alexengrigdev@gmail.com>
2019-12-07 22:14:35 +0300

    Add solution

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,10 +1,34 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
+    static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Card:");
-        System.out.println("purchase");
-        System.out.println("Definition:");
-        System.out.println("buy");
+        String term = SCANNER.nextLine();
+        String definition = SCANNER.nextLine();
+        Card card = new Card(term, definition);
+        String answer = SCANNER.nextLine();
+        boolean right = card.right(answer);
+        if (right) {
+            System.out.println("Your answer is right!");
+        } else {
+            System.out.println("Your answer is wrong...");
+        }
+    }
+
+    static class Card{
+        final String term;
+        final String definition;
+
+        Card(String term, String definition) {
+            this.term = term;
+            this.definition = definition;
+        }
+
+        boolean right(String answer) {
+            return definition.equals(answer);
+        }
     }
 }
