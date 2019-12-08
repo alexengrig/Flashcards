fc4725e3365bc174524ff1ad7bde692d7ce39f99
Grig Alex <alexengrigdev@gmail.com>
2019-12-08 17:46:14 +0300

    Add "Maximum element in a matrix" problem solution

---
+++ Problems/Maximum element in a matrix/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] matrix = new int[n][m];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        int maxI = 0;
+        int maxJ = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                    maxI = i;
+                    maxJ = j;
+                }
+            }
+        }
+        System.out.println(maxI + " " + maxJ);
+    }
+}
