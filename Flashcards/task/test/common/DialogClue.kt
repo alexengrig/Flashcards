5274a0b35e9c2a9e39b6b21785dfbb308ebef949
Grig Alex <alexengrigdev@gmail.com>
2019-12-14 16:50:49 +0300

    Add problem solutions

--- Flashcards/task/test/common/DialogClue.kt
+++ Flashcards/task/test/common/DialogClue.kt
@@ -44,7 +44,7 @@ class OutputLine(val checker: (text: String, ctx: Context) -> CheckResult) : Phr
  * we need to pass all inputs first, and then start checking outputs. */
 fun user(text: String, updateContext: (ctx: Context) -> Unit = {}) = UserLine(text, updateContext)
 
-fun anyLine() = OutputLine { _, _ -> CheckResult.TRUE }
+fun anyLine(updateContext: CtxUpdate = {}) = OutputLine { _, ctx -> CheckResult.TRUE.also { updateContext(ctx) } }
 
 fun containing(
         vararg parts: String,
@@ -105,8 +105,8 @@ class DialogClue(private val phrases: List<PhraseLine>) {
                         return wrongOutputSizeFeedback()
                     }
 
-                    val line: String = lineIter.next()
-                    val result = phraseLine.checker(line, context)
+
+                    val result = phraseLine.checker(lineIter.next(), context)
                     if (result != CheckResult.TRUE) {
                         // add the test start to output:
                         val trimmedLines = lines.take(lineIter.nextIndex())
@@ -149,9 +149,10 @@ class DialogClue(private val phrases: List<PhraseLine>) {
     }
 }
 
-fun dialogTest(vararg phrases: Phrase): TestCase<DialogClue> {
+fun dialogTest(vararg phrases: Phrase, consoleArgs: Array<String> = emptyArray()): TestCase<DialogClue> {
     val dialogClue = DialogClue(phrases.flatMap { it.toPhraseLines() })
     return TestCase<DialogClue>()
             .setInput(dialogClue.generateInput())
             .setAttach(dialogClue)
+            .addArguments(*consoleArgs)
 }
